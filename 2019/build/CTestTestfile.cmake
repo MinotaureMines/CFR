ed3f1505e0fe9272efea43f082ccfecf93e9f9eb
MinotaureMines <minotaure@mines-paristech.fr>
2019-06-26 23:01:26 +0200

    Delete 2019

--- 2019/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tg/Github/CFR/2019/src
-# Build directory: /home/tg/Github/CFR/2019/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("rplidar_ros")
--- 2019/build/rplidar_ros/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tg/Github/CFR/2019/src/rplidar_ros
-# Build directory: /home/tg/Github/CFR/2019/build/rplidar_ros
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
